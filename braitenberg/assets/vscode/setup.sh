2646760ae041eeebde212a654005250378363243
Andrea Censi <acensi@ethz.ch>
2022-11-28 23:32:57 +0100

    config

--- braitenberg/assets/vscode/setup.sh
+++ braitenberg/assets/vscode/setup.sh
@@ -1,4 +1,4 @@
 # open README.md at startup
 VSCODE_USER_SETTINGS=/home/duckie/.local/share/code-server/User/settings.json
-OUT=$(jq '. + {"workbench.startupEditor": "readme"}' "${VSCODE_USER_SETTINGS}")
+OUT=$(jq '. + {"workbench.startupEditor": "readme", "jupyter.widgetScriptSources": ["jsdelivr.com", "unpkg.com"]}' "${VSCODE_USER_SETTINGS}")
 echo "${OUT}" > "${VSCODE_USER_SETTINGS}"
